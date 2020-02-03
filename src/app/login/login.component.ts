d6b6b1724475ac5efbcede3c638f3c0f3b8a3c1a
webmaster-pnn <machadopnn@gmail.com>
2020-02-03 01:12:50 -0300

    configurando o formulario de login para passar variaveis

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SSL_OP_NETSCAPE_CHALLENGE_BUG } from 'constants';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  usuario: any = {
+    nip: '',
+    senha: ''
+  }
+  onSubmit(form){
+    console.log(form);
+    console.log(this.usuario)
+  }
 
   constructor() { }
 
