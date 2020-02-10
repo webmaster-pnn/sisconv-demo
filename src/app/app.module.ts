53d80127b7e2d3f03473507e385b0fd555ec0cd6
webmaster-pnn <machadopnn@gmail.com>
2020-02-10 02:25:15 -0300

    adicionando logo svg e configurando rotas do navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularSvgIconModule } from 'angular-svg-icon'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +19,9 @@ import { AuthService } from './login/auth.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    AngularSvgIconModule
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
