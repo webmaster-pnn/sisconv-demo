2d92be8749ca67ff92c49be2cf11e144412f1e14
webmaster-pnn <machadopnn@gmail.com>
2020-02-23 21:08:43 -0300

    comentarios em relacao ao codigo da pagina proprietarios

--- src/app/proprietarios/proprietarios.component.ts
+++ src/app/proprietarios/proprietarios.component.ts
@@ -1,5 +1,7 @@
-import { Proprietarios } from './../model/proprietarios';
 import { Component, OnInit, ViewChild } from '@angular/core';
+// componente
+import { Proprietarios } from './../model/proprietarios';
+// material design
 import { MatTableDataSource } from '@angular/material/table';
 import {MatPaginator, MatPaginatorIntl} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
@@ -10,19 +12,26 @@ import {MatSort} from '@angular/material/sort';
   styleUrls: ['./proprietarios.component.css']
 })
 export class ProprietariosComponent extends MatPaginatorIntl implements OnInit  {
+
+  // instancia de proprietario
   proprietario: Proprietarios[] = [
     { "id": 1,"grad": "MN", "nome": "Carlos Henrique Pereira Machado Júnior", "nip": "16115597", "setor": "CPD", "cnh":"2251522-55", "editar": ""},
     { "id": 2,"grad": "CB", "nome": "Carlos Henrique Pereira Machado Júnior", "nip": "16115597", "setor": "CPD", "cnh":"2251522-55", "editar": ""},
     { "id": 3,"grad": "MN", "nome": "Carlos Henrique Pereira Machado Júnior", "nip": "16115597", "setor": "CPD", "cnh":"2251522-55", "editar": ""}
-    
   ];
+  // variaveis
   tabela_vazia: boolean = false;
+
+  // colunas da tabela
   displayedColumns: string[] = ['grad','nome', 'nip', 'setor','cnh', 'editar'];
+  // criando um novo objeto da tabela data source e passando o proprietario como parametro
   dataSource = new MatTableDataSource(this.proprietario);
 
+  // permite a atualização em tempo real da tabela.
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
+  // função para aplicar o filtro na tabela
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
@@ -34,6 +43,7 @@ export class ProprietariosComponent extends MatPaginatorIntl implements OnInit  
   }
 
   constructor() { 
+    // herdando objetos da classe pai e utilizando para alterar elementos do paginator
     super();
     // paginator tradução
     this.itemsPerPageLabel = 'Itens por pagina';
@@ -43,7 +53,6 @@ export class ProprietariosComponent extends MatPaginatorIntl implements OnInit  
       }
       length = Math.max(length, 0);
       const startIndex = page * pageSize;
-      // If the start index exceeds the list length, do not try and fix the end index to the end.
       const endIndex = startIndex < length ?
         Math.min(startIndex + pageSize, length) :
         startIndex + pageSize;
@@ -52,8 +61,10 @@ export class ProprietariosComponent extends MatPaginatorIntl implements OnInit  
   }
 
   ngOnInit() {
+    // atribuindo ao proprietario a paginação e os filtros.
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+    // verificando quando o objeto estiver vazio
     if(this.proprietario.length == 0){
       this.tabela_vazia = true;
     }
